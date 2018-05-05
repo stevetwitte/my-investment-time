c4b227f54ada167a11dbfd1cb6b3a7971700e8ee
Stephen Taylor Witte <stevetwitte@gmail.com>
2018-05-05 15:03:55 -0400

    switches to rspec and adds tailwind and jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery3
 //= require_tree .
+
+function toggleMenu() {
+    $('#mainMenu').toggleClass('menu-hidden');
+}
