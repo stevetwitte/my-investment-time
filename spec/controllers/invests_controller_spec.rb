bc3deedeb5dac1108cd73a76d515020b5faf477a
Stephen Taylor Witte <stevetwitte@gmail.com>
2018-05-20 11:11:24 -0400

    fixes invests controller spec and creates specs for index and new view

--- spec/controllers/invests_controller_spec.rb
+++ spec/controllers/invests_controller_spec.rb
@@ -3,17 +3,54 @@ require 'rails_helper'
 RSpec.describe InvestsController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    before :each do
+      10.times do
+        create :invest
+      end
+
       get :index
-      expect(response).to have_http_status(:ok)
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status :ok
+    end
+
+    it "returns invests in instance variable" do
+      expect(assigns(:invests).length).to be 10
     end
   end
 
   describe "GET #new" do
-    it "returns http success" do
+    before :each do
+      sign_in
       get :new
-      expect(response).to have_http_status(:ok)
+    end
+
+    it "returns http found" do
+      expect(response).to have_http_status :ok
+    end
+
+    it "has an instance variable of a new invest" do
+      expect(assigns(:invest)).to be_a_new Invest
     end
   end
 
+  describe "POST #create" do
+    before :each do
+      sign_in
+      post :create, params: { invest: FactoryBot.attributes_for(:invest) }
+    end
+
+    it "returns http found" do
+      expect(response).to have_http_status :found
+    end
+
+    it "redirects to the root url" do
+      expect(response).to redirect_to root_url
+    end
+
+    it "creates an invest" do
+      expect(Invest.all.length).to eql 1
+    end
+  end
 end
--- spec/views/invests/new.html.erb_spec.rb
+++ spec/views/invests/new.html.erb_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "invests/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "with a logged in user" do
+    before :each do
+      sign_in
+      assign(:invest, build(:invest))
+    end
+
+    it "displays a new form" do
+      render
+
+      expect(rendered).to match /new investment/
+    end
+  end
 end
