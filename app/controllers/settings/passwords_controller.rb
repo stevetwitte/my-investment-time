229c2fad066b2043b88755e62cab34d8ade8a34d
Stephen Taylor Witte <stevetwitte@gmail.com>
2018-06-02 14:49:51 -0400

    adds passwords controller, view and spec with routes and updated localization for english

---
+++ app/controllers/settings/passwords_controller.rb
@@ -0,0 +1,26 @@
+module Settings
+  class PasswordsController < ApplicationController
+    def edit
+      @user = current_user
+    end
+
+    def update
+      @user = current_user
+
+      if @user.authenticated?(password_params[:current_password]) &&
+         @user.update(password: password_params[:new_password])
+        flash[:notice] = 'successfully updated password'
+        redirect_to edit_settings_password_path
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def password_params
+      params.require(:user).permit(:current_password,
+                                   :new_password)
+    end
+  end
+end
