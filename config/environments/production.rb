0d250a5b11e51f3c18630143e868673506bd7a67
Stephen Taylor Witte <stevetwitte@gmail.com>
2018-06-17 10:14:26 -0400

    email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => ENV['EMAIL_HOST'] }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
