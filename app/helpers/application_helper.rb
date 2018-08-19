668f6ee25fbd5caf0952699cc6dab77653e8b05f
Stephen Witte <stevetwitte@gmail.com>
2018-08-19 14:48:52 -0400

    adds liked helper to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def following?(user:, invest:)
     user.following_invests.include?(invest)
   end
+
+  def liked?(user:, invest:)
+    user.liked_invests.include?(invest)
+  end
 end
