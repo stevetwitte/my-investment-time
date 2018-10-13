8a37a10337bb52b7709096b9676ae66bb4cfc8fb
Stephen Taylor Witte <stevetwitte@gmail.com>
2018-10-13 13:42:46 -0400

    fix styling

--- spec/factories/invests.rb
+++ spec/factories/invests.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     title { Faker::Lorem.sentence }
     body { Faker::Lorem.paragraph(2) + "#tagone" + "#tagtwo" }
     user { create(:user) }
-    links { { "homepage": "https://www.github.com",
-              "github": "https://www.github.com" } }
+    links { { homepage: "https://www.github.com",
+              github: "https://www.github.com" } }
   end
 end
